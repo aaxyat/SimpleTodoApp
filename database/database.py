4be69c4bc1cf391ea880a71de5579483c49b8845
Ayush Bhattarai <aaxyat@gmail.com>
2024-03-09 21:45:52 +0545

    implement postgres

--- database/database.py
+++ database/database.py
@@ -1,11 +1,13 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
+import dotenv
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./todosapp.db"
+environ = dotenv.dotenv_values()
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={
-                       "check_same_thread": False})
+SQLALCHEMY_DATABASE_URL = environ.get("DATABASE_URL")
+
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
